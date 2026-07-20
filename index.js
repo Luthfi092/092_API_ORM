e2a11440ff8457ced8c45348001257af4a79e9b3
upi1682 <upi538287@gmail.com>
2026-07-20 15:34:27 +0700

    app.delete

--- index.js
+++ index.js
@@ -57,3 +57,17 @@ app.put('/komik/:id', async (req, res) => {
     }
 });
 
+app.delete('/komik/:id', async (req, res) => {
+    const { id } = req.params; 
+    try {
+        const komik = await db.Komik.findByPk(id);
+        if (!komik) {
+            return res.status(404).send({ error: 'Komik not found' });
+        }
+        await komik.destroy();
+        res.send({ message: 'Komik deleted successfully' });
+    } catch (err) {
+        res.send({ error: err.message });
+        res.status(500).send({ error: 'An error occurred while deleting the komik' });
+    }
+});
