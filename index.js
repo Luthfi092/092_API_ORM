7cef7ba541d1fbf86f704cc33b95d6bfa44784ed
upi1682 <upi538287@gmail.com>
2026-07-20 15:33:16 +0700

    app.post

--- index.js
+++ index.js
@@ -12,5 +12,24 @@ app.listen(PORT, () => {
     console.log('Server started on port 3000');
 })
 
+db.sequelize.sync()
+    .then((result) => {
+        app.listen(3000, () => {
+            console.log('Server started');
+        })
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
+    app.post('/komik', async (req, res) => {
+        const { title, author } = req.body;
+        try {
+            const komik = await db.Komik.create({ data });
+            res.send(komik);
+        } catch (err) {
+            res.send({ error: err.message });
+        }
+    });
 
 
